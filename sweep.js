54a7990effb95a28a17da8ea59eabeb778233745
Markus Popp <git@mpopp.net>
2016-12-03 05:05:51 +0100

    Check for domains without dot in front

--- sweep.js
+++ sweep.js
@@ -15,7 +15,7 @@ function clearCookies(cookies) {
         for (var d of domainsArray) {
             d = d.trim();
 
-            if (cookie.domain.endsWith('.' + d)) {
+            if (cookie.domain === d || cookie.domain.endsWith('.' + d)) {
                 // keep cookie
                 console.log("KEEP " + cookie.domain + " --- " + cookie.name);
                 deleteCookie = false;
