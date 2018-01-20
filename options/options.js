b79f7e24812c7113b82599c9c39bbd850aba8a5e
bytebuster463 <bytebuster463@gmail.com>
2018-01-20 23:18:34 +0200

    fixed mpopp75/cookie-sweep#5

--- options/options.js
+++ options/options.js
@@ -18,5 +18,37 @@ function restoreOptions() {
   getting.then(setCurrentChoice, onError);
 }
 
+function runMain() {
+  // clear old progress if any
+  var progressOld = document.getElementById("runNowResult");
+  var progress = progressOld.cloneNode(false);
+  progressOld.parentNode.replaceChild(progress, progressOld);
+  // Show progress indicator
+  var paraP = document.createElement("p");
+  var nodeP = document.createTextNode("Processing...");
+  paraP.appendChild(nodeP);
+  progress.appendChild(paraP);
+
+  clearCookies()
+  .then(stats => {
+    console.log("count.kept: " + stats.kept);
+    console.log("count.removed: " + stats.removed);
+
+    paraP.remove();
+
+    var para1 = document.createElement("p");
+    var node1 = document.createTextNode(parseInt(stats.kept) + " Cookies kept.");
+    para1.appendChild(node1);
+
+    var para2 = document.createElement("p");
+    var node2 = document.createTextNode(parseInt(stats.removed) + " Cookies removed.");
+    para2.appendChild(node2);
+
+    progress.appendChild(para1);
+    progress.appendChild(para2);
+  });
+}
+
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
+document.querySelector("#runNow").addEventListener("click", runMain);
--- popup/info-panel.js
+++ popup/info-panel.js
@@ -1,86 +1,33 @@
-var domains = null;
-var kept = 0;
-var removed = 0;
-
-function clearCookies(cookies) {
-
-    var domainsArray = domains.split(",").map(d => d.trim());
-
-    for (var cookie of cookies) {
-        var deleteCookie = true;
-        for (var d of domainsArray) {
-            if (cookie.domain === d || cookie.domain.endsWith('.' + d)) {
-                // keep cookie
-                console.log("KEEPING " + cookie.domain + cookie.path + " --- " + cookie.name);
-                deleteCookie = false;
-                kept++;
-                break;
-            }
-        }
-
-        if (deleteCookie) {
-            var removingHttp = browser.cookies.remove({
-                url: "http://" + cookie.domain + cookie.path,
-                name: cookie.name
-            });
-            removingHttp.then(onCookieRemoved, onCookieError);
-
-            var removingHttps = browser.cookies.remove({
-                url: "https://" + cookie.domain + cookie.path,
-                name: cookie.name
-            });
-            removingHttps.then(onCookieRemoved, onCookieError);
-            console.log("REMOVING " + cookie.domain + cookie.path + " --- " + cookie.name);
-            removed++;
-        }
-    }
-
-    console.log("count.kept: " + kept);
-    console.log("count.removed: " + removed);
-
-    var para1 = document.createElement("p");
-    var node1 = document.createTextNode(parseInt(kept) + " Cookies kept.");
-    para1.appendChild(node1);
-
-    var para2 = document.createElement("p");
-    var node2 = document.createTextNode(parseInt(removed) + " Cookies removed.");
-    para2.appendChild(node2);
-
-    document.getElementById("output").appendChild(para1);
-    document.getElementById("output").appendChild(para2);
-}
-
-function onStorageGot(item) {
-    domains = item.domains;
-
-    var getting = browser.cookies.getAll({});
-    getting.then(clearCookies);
-}
-
-function onStorageError(error) {
-  console.log(`Error: ${error}`);
-}
-
-function onCookieRemoved(cookie) {
-    try {
-        console.log("REMOVED " + cookie.domain + cookie.path + " --- " + cookie.name);
-    } catch (e) {}
-    return;
-}
-
-function onCookieError(error) {
-    console.log(`Error removing cookie: ${error}`);
-}
-
-function storageChanged() {
+function onStorageChanged() {
     runMain();
 }
 
 function runMain() {
-    var getting = browser.storage.local.get("domains");
-    getting.then(onStorageGot, onStorageError);
+    clearCookies()
+    .then(stats => {
+      console.log("count.kept: " + stats.kept);
+      console.log("count.removed: " + stats.removed);
+
+      document.getElementById("processing").remove();
+
+      var para1 = document.createElement("p");
+      var node1 = document.createTextNode(parseInt(stats.kept) + " Cookies kept.");
+      para1.appendChild(node1);
+
+      var para2 = document.createElement("p");
+      var node2 = document.createTextNode(parseInt(stats.removed) + " Cookies removed.");
+      para2.appendChild(node2);
+
+      document.getElementById("output").appendChild(para1);
+      document.getElementById("output").appendChild(para2);
+    });
 }
 
-runMain();
+function handleMessage(request, sender, sendResponse) {
 
-browser.storage.onChanged.addListener(storageChanged);
+    sendResponse(statistics);
+}
+
+document.addEventListener("DOMContentLoaded", runMain);
+browser.runtime.onMessage.addListener(handleMessage);
+browser.storage.onChanged.addListener(onStorageChanged);
