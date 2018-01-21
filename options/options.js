f0906efd2f594d4db59ccc7b7d8d0b084bd6f5be
bytebuster463 <bytebuster463@gmail.com>
2018-01-21 06:22:03 +0200

    Added autosave for Options; Display results for Action button in Options form; Cosmetic improvements

--- options/options.js
+++ options/options.js
@@ -1,4 +1,12 @@
+var saveTimer;
+/* Save options async.
+   The operation is delayed 1000ms so that it did not save on each key */
 function saveOptions(e) {
+  if(saveTimer) { clearTimeout(saveTimer); }
+  saveTimer = setTimeout(saveOptionsSync, 1000);
+};
+/* Save options syncchronously (immediately). */
+function saveOptionsSync() {
   browser.storage.local.set({
     domains: document.querySelector("#domains").value
   });
@@ -20,7 +28,7 @@ function restoreOptions() {
 
 function runMain() {
   // clear old progress if any
-  var progressOld = document.getElementById("runNowResult");
+  var progressOld = document.getElementById("actionResult");
   var progress = progressOld.cloneNode(false);
   progressOld.parentNode.replaceChild(progress, progressOld);
   // Show progress indicator
@@ -31,24 +39,26 @@ function runMain() {
 
   clearCookies()
   .then(stats => {
-    console.log("count.kept: " + stats.kept);
     console.log("count.removed: " + stats.removed);
+    console.log("count.kept: " + stats.kept);
 
     paraP.remove();
 
     var para1 = document.createElement("p");
-    var node1 = document.createTextNode(parseInt(stats.kept) + " Cookies kept.");
+    var node1 = document.createTextNode(parseInt(stats.kept) + " cookies kept.");
     para1.appendChild(node1);
 
     var para2 = document.createElement("p");
-    var node2 = document.createTextNode(parseInt(stats.removed) + " Cookies removed.");
+    var node2 = document.createTextNode(parseInt(stats.removed) + " cookies removed.");
     para2.appendChild(node2);
 
-    progress.appendChild(para1);
     progress.appendChild(para2);
+    progress.appendChild(para1);
   });
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+// document.querySelector("#save").addEventListener("click", saveOptions);
+document.querySelector("#domains").addEventListener("input", saveOptions); // Autosave
+window.addEventListener("blur", saveOptionsSync); // Autosave on window deactivate
 document.querySelector("#runNow").addEventListener("click", runMain);
--- popup/info-panel.js
+++ popup/info-panel.js
@@ -5,29 +5,23 @@ function onStorageChanged() {
 function runMain() {
     clearCookies()
     .then(stats => {
-      console.log("count.kept: " + stats.kept);
       console.log("count.removed: " + stats.removed);
+      console.log("count.kept: " + stats.kept);
 
       document.getElementById("processing").remove();
 
       var para1 = document.createElement("p");
-      var node1 = document.createTextNode(parseInt(stats.kept) + " Cookies kept.");
+      var node1 = document.createTextNode(parseInt(stats.kept) + " cookies kept.");
       para1.appendChild(node1);
 
       var para2 = document.createElement("p");
-      var node2 = document.createTextNode(parseInt(stats.removed) + " Cookies removed.");
+      var node2 = document.createTextNode(parseInt(stats.removed) + " cookies removed.");
       para2.appendChild(node2);
 
-      document.getElementById("output").appendChild(para1);
       document.getElementById("output").appendChild(para2);
+      document.getElementById("output").appendChild(para1);
     });
 }
 
-function handleMessage(request, sender, sendResponse) {
-
-    sendResponse(statistics);
-}
-
 document.addEventListener("DOMContentLoaded", runMain);
-browser.runtime.onMessage.addListener(handleMessage);
 browser.storage.onChanged.addListener(onStorageChanged);
